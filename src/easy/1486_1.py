51aa19b5ae09cd24a994a50e3259d9eb7ab3ad60
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2021-04-21 15:40:49 +0530

    Added Time&Space Complexities

--- src/easy/1486_1.py
+++ src/easy/1486_1.py
@@ -3,3 +3,12 @@ class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         nums = [start+2*i for i in range(n)]
         return reduce(lambda x,y:x^y,nums)
+
+# Time Complexity:
+# O(n**2)
+# First we need to iterate through n elements to form our array/list
+# Second we need to iterate through all those elements again for calculating the XOR (the reduce function)
+#
+# Space Complexity:
+# O(n)
+# Memory assigned depends upon the size of the list/array n 
